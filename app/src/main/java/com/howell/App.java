9eeebc8456565a2b3bd29a673aaae944c4c49f8b
Bjelijah <elijah@live.cn>
2017-10-20 11:24:43 +0800

    new lamp version

--- app/src/main/java/com/howell/App.java
+++ app/src/main/java/com/howell/App.java
@@ -1,6 +1,9 @@
 package com.howell;
 
 import android.app.Application;
+import android.content.Context;
+import android.support.multidex.MultiDex;
+import android.support.multidex.MultiDexApplication;
 
 import com.howell.pushlibrary.DaemonEnv;
 import com.howell.service.MyService;
@@ -9,7 +12,14 @@ import com.howell.service.MyService;
  * Created by Administrator on 2017/6/8.
  */
 
-public class App extends Application {
+public class App extends MultiDexApplication {
+
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+        MultiDex.install(this);
+    }
+
     @Override
     public void onCreate() {
         super.onCreate();
